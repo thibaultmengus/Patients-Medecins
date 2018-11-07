e6b09b3cfb2ce97493defd3cc2a12b898711ff0e
Thibaut Mengus <thibaut_mengus@hotmail.fr>
2018-11-07 14:07:39 +0100

    changement du fichier de creation SQL

--- BaseDeDonnees/create_PatientsMedecins.sql
+++ BaseDeDonnees/create_PatientsMedecins.sql
@@ -2,28 +2,28 @@
 # Table: Personne
 #------------------------------------------------------------
 
-CREATE TABLE personne(
+CREATE TABLE Personne(
         idPersonne Int  Auto_increment NOT NULL,
+		mail	   Varchar (50) NOT NULL,
+		password   Varchar (255) NOT NULL,
         nom        Varchar (50) NOT NULL,
         prenom     Varchar (50) NOT NULL,
         telephone  Varchar (12) NOT NULL,
-		mail	   Varchar (50) NOT NULL,
-		password   Varchar (255) NOT NULL,
 
 	CONSTRAINT Personne_PK PRIMARY KEY (idPersonne)
 );
 
 
 #------------------------------------------------------------
-# Table: Médecin
+# Table: Medecin
 #------------------------------------------------------------
 
-CREATE TABLE medecin(
-        idMedecin  Int NOT NULL,
-        specialite Varchar (50) NOT NULL,
-        adresse    Varchar (50) NOT NULL,
-        cp         Int NOT NULL,
-        ville      Varchar (50) NOT NULL,
+CREATE TABLE Medecin(
+        idMedecin   Int NOT NULL,
+        specialite  Varchar (50) NOT NULL,
+        adresse     Varchar (50) NOT NULL,
+        codePostal  Int NOT NULL,
+        ville       Varchar (50) NOT NULL,
 
 	CONSTRAINT Medecin_PK PRIMARY KEY (idMedecin),
 	CONSTRAINT Medecin_Personne_FK FOREIGN KEY (idMedecin) REFERENCES Personne(idPersonne)
@@ -34,13 +34,12 @@ CREATE TABLE medecin(
 # Table: Consultation
 #------------------------------------------------------------
 
-CREATE TABLE consultation(
-        idMedecin          Int NOT NULL,
-        creneauHoraire     Datetime NOT NULL,
-        idPatient 		   Int,
+CREATE TABLE Consultation(
+        idMedecin      Int NOT NULL,
+        creneauHoraire Datetime NOT NULL,
+        idPatient 	   Int,
 
 	CONSTRAINT Consultation_PK PRIMARY KEY (idMedecin, creneauHoraire),
 	CONSTRAINT Consultation_Medecin_FK FOREIGN KEY (idMedecin) REFERENCES Medecin(idMedecin),
 	CONSTRAINT Consultation_Personne_FK FOREIGN KEY (idPatient) REFERENCES Personne(idPersonne)
 );
-
