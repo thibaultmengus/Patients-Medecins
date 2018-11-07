66098d804651c2ec49b751220b0101d644fd10a0
Enes <enes.uyar@pm.me>
2018-11-07 12:11:23 +0100

    Fix SQL Scripts

--- BaseDeDonnees/remplir_PatientsMedecins.sql
+++ BaseDeDonnees/remplir_PatientsMedecins.sql
@@ -1,10 +1,18 @@
-INSERT INTO medecin(specialite,adresse,cp,ville) VALUES ('généraliste','12 rue rivoli'           ,'75016','Paris'),
-INSERT INTO medecin(specialite,adresse,cp,ville) VALUES ('généraliste','39 rue danielle casanova','92000','Neuilly-sur-Seine'),
-INSERT INTO medecin(specialite,adresse,cp,ville) VALUES ('généraliste','67 avenue des malandrins','75019','Paris'),
+INSERT INTO personne(nom,prenom,telephone) VALUES ('sartre'   ,'jean'   ,'+33612256876');
+INSERT INTO personne(nom,prenom,telephone) VALUES ('menot'    ,'bastien','+33648853512');
+INSERT INTO personne(nom,prenom,telephone) VALUES ('lihoreau' ,'david'  ,'+33771690013');
+INSERT INTO personne(nom,prenom,telephone) VALUES ('skywalker','luc'    ,'+33697488522');
+INSERT INTO personne(nom,prenom,telephone) VALUES ('sebastien','patrick','+33669430034');
+INSERT INTO personne(nom,prenom,telephone) VALUES ('rabiot'   ,'adrien' ,'+33769332655');
 
-INSERT INTO personne(nom,prenom,telephone) VALUES ('sartre'   ,'jean'   ,'+33612256876'),
-INSERT INTO personne(nom,prenom,telephone) VALUES ('menot'    ,'bastien','+33648853512'),
-INSERT INTO personne(nom,prenom,telephone) VALUES ('lihoreau' ,'david'  ,'+33771690013'),
-INSERT INTO personne(nom,prenom,telephone) VALUES ('skywalker','luc'    ,'+33697488522'),
-INSERT INTO personne(nom,prenom,telephone) VALUES ('sebastien','patrick','+33669430034'),
-INSERT INTO personne(nom,prenom,telephone) VALUES ('rabiot'   ,'adrien' ,'+33769332655')
+INSERT INTO medecin(idMedecin,specialite,adresse,cp,ville)
+	SELECT idPersonne,'généraliste','12 rue rivoli','75016','Paris'
+	FROM personne WHERE nom="sartre" AND prenom="jean";
+
+INSERT INTO medecin(idMedecin,specialite,adresse,cp,ville)
+	SELECT idPersonne,'généraliste','33 avenue charles de gaulle','92000','Nanterre'
+	FROM personne WHERE nom="menot" AND prenom="bastien";
+
+INSERT INTO medecin(idMedecin,specialite,adresse,cp,ville)
+	SELECT idPersonne,'généraliste','67 avenue des malandrins','75019','Paris'
+	FROM personne WHERE nom="lihoreau" AND prenom="david";
