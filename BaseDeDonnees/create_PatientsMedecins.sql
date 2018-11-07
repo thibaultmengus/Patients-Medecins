e682663d1bee7d269d672772f1d68e23905bc20d
Okami <pierrick.garcia@hotmail.fr>
2018-11-07 15:22:58 +0100

    Redesign database modelisation

--- BaseDeDonnees/create_PatientsMedecins.sql
+++ BaseDeDonnees/create_PatientsMedecins.sql
@@ -1,16 +1,31 @@
 #------------------------------------------------------------
+#        Script MySQL.
+#------------------------------------------------------------
+
+
+#------------------------------------------------------------
 # Table: Personne
 #------------------------------------------------------------
 
 CREATE TABLE Personne(
-        idPersonne Int  Auto_increment NOT NULL,
-		mail	   Varchar (50) NOT NULL,
-		password   Varchar (255) NOT NULL,
-        nom        Varchar (50) NOT NULL,
-        prenom     Varchar (50) NOT NULL,
-        telephone  Varchar (12) NOT NULL,
+        idPersonne Int  Auto_increment  NOT NULL ,
+        mail       Varchar (50) NOT NULL ,
+        password   Varchar (255) NOT NULL ,
+        nom        Varchar (50) NOT NULL ,
+        prenom     Varchar (50) NOT NULL ,
+        telephone  Varchar (12) NOT NULL
+	,CONSTRAINT Personne_PK PRIMARY KEY (idPersonne)
+);
 
-	CONSTRAINT Personne_PK PRIMARY KEY (idPersonne)
+
+#------------------------------------------------------------
+# Table: Specialite
+#------------------------------------------------------------
+
+CREATE TABLE Specialite(
+        idSpecialite Int  Auto_increment  NOT NULL ,
+        specialite   Varchar (255) NOT NULL
+	,CONSTRAINT Specialite_PK PRIMARY KEY (idSpecialite)
 );
 
 
@@ -19,14 +34,15 @@ CREATE TABLE Personne(
 #------------------------------------------------------------
 
 CREATE TABLE Medecin(
-        idMedecin   Int NOT NULL,
-        specialite  Varchar (50) NOT NULL,
-        adresse     Varchar (50) NOT NULL,
-        codePostal  Int NOT NULL,
-        ville       Varchar (50) NOT NULL,
+        idMedecin    Int NOT NULL ,
+        adresse      Varchar (255) NOT NULL ,
+        codePostal   Int NOT NULL ,
+        ville        Varchar (50) NOT NULL ,
+        idSpecialite Int
+	,CONSTRAINT Medecin_PK PRIMARY KEY (idMedecin)
 
-	CONSTRAINT Medecin_PK PRIMARY KEY (idMedecin),
-	CONSTRAINT Medecin_Personne_FK FOREIGN KEY (idMedecin) REFERENCES Personne(idPersonne)
+	,CONSTRAINT Medecin_Personne_FK FOREIGN KEY (idMedecin) REFERENCES Personne(idPersonne)
+	,CONSTRAINT Medecin_Specialite0_FK FOREIGN KEY (idSpecialite) REFERENCES Specialite(idSpecialite)
 );
 
 
@@ -35,11 +51,11 @@ CREATE TABLE Medecin(
 #------------------------------------------------------------
 
 CREATE TABLE Consultation(
-        idMedecin      Int NOT NULL,
-        creneauHoraire Datetime NOT NULL,
-        idPatient 	   Int,
+        creneauHoraire     Datetime NOT NULL ,
+        idMedecin          Int NOT NULL ,
+        idPersonne		   Int
+	,CONSTRAINT Consultation_PK PRIMARY KEY (creneauHoraire)
 
-	CONSTRAINT Consultation_PK PRIMARY KEY (idMedecin, creneauHoraire),
-	CONSTRAINT Consultation_Medecin_FK FOREIGN KEY (idMedecin) REFERENCES Medecin(idMedecin),
-	CONSTRAINT Consultation_Personne_FK FOREIGN KEY (idPatient) REFERENCES Personne(idPersonne)
+	,CONSTRAINT Consultation_Medecin_FK FOREIGN KEY (idMedecin) REFERENCES Medecin(idMedecin)
+	,CONSTRAINT Consultation_Personne_FK FOREIGN KEY (idPersonne) REFERENCES Personne(idPersonne)
 );
--- BaseDeDonnees/remplir_PatientsMedecins.sql
+++ BaseDeDonnees/remplir_PatientsMedecins.sql
@@ -5,14 +5,14 @@ INSERT INTO personne(nom,prenom,telephone,mail,password) VALUES ('skywalker','lu
 INSERT INTO personne(nom,prenom,telephone,mail,password) VALUES ('sebastien','patrick','+33669430034','etonfaittournerlesserviettes@gmail.com','123456');
 INSERT INTO personne(nom,prenom,telephone,mail,password) VALUES ('rabiot'   ,'adrien' ,'+33769332655','adrienrabbit@gmail.com','qwerty');
 
-INSERT INTO medecin(idMedecin,specialite,adresse,cp,ville)
-	SELECT idPersonne,'généraliste','12 rue rivoli','75016','Paris'
-	FROM personne WHERE mail="jean.sartre@gmail.com"
+INSERT INTO medecin(idMedecin,adresse,cp,ville)
+	SELECT idPersonne,'12 rue rivoli','75016','Paris'
+	FROM personne WHERE mail="jean.sartre@gmail.com";
 
-INSERT INTO medecin(idMedecin,specialite,adresse,cp,ville)
-	SELECT idPersonne,'généraliste','33 avenue charles de gaulle','92000','Nanterre'
-	FROM personne WHERE mail="bastien.menot@gmail.com"
+INSERT INTO medecin(idMedecin,adresse,cp,ville)
+	SELECT idPersonne,'33 avenue charles de gaulle','92000','Nanterre'
+	FROM personne WHERE mail="bastien.menot@gmail.com";
 
-INSERT INTO medecin(idMedecin,specialite,adresse,cp,ville)
-	SELECT idPersonne,'généraliste','67 avenue des malandrins','75019','Paris'
-	FROM personne WHERE mail="etonfaittournerlesserviettes@gmail.com"
+INSERT INTO medecin(idMedecin,adresse,cp,ville)
+	SELECT idPersonne,'67 avenue des malandrins','75019','Paris'
+	FROM personne WHERE mail="etonfaittournerlesserviettes@gmail.com";
