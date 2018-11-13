ff3bd52128eb3018f1a40db77aaa5cd8f7f783d7
Okami <pierrick.garcia@hotmail.fr>
2018-11-13 14:32:27 +0100

    Add rdv php requests

--- fonctionsBdd.php
+++ fonctionsBdd.php
@@ -49,11 +49,86 @@ class fonctionsBdd
 			$data2 = $this->execute($requete);
             $_SESSION['estMedecin'] = ! empty($data2['idMedecin']);
             $_SESSION['estConnecte'] = true;
+            $_SESSION['idPersonne'] = $data['idPersonne'];
             $_SESSION['identifiant'] = $data['nom'] . ' ' . $data['prenom'];
             return true;
         }
         else
             return false;
     }
+	
+	/**
+     * @param $idMedecin ID du médecin du rendez-vous pris
+     * @param $creneauHoraire Créneau horaire du rendez-vous pris
+     * Réserve une consultation pour le patient connecté
+     */
+	public function ajouteRendezVousPatient($idMedecin, $creneauHoraire)
+	{
+		$requete = $this->sendRequest('UPDATE Consultation SET idPersonne = :idPersonne WHERE idMedecin=:idMedecin AND creneauHoraire=:creneauHoraire')
+		$requete->bindValue(':idPersonne', $_SESSION['idPersonne']);
+		$requete->bindValue(':idMedecin', $idMedecin);
+		$requete->bindValue(':creneauHoraire', $creneauHoraire);
+		$this->execute($requete);
+	}
+	
+	/**
+     * @param $idMedecin ID du médecin du rendez-vous pris
+     * @param $creneauHoraire Créneau horaire du rendez-vous pris
+     * Supprime une consultation pour le patient connecté
+     */
+	public function supprimeRendezVousPatient($idMedecin, $creneauHoraire)
+	{
+		$requete = $this->sendRequest('UPDATE Consultation SET idPersonne = NULL WHERE idMedecin=:idMedecin AND creneauHoraire=:creneauHoraire')
+		$requete->bindValue(':idPersonne', $_SESSION['idPersonne']);
+		$requete->bindValue(':idMedecin', $idMedecin);
+		$requete->bindValue(':creneauHoraire', $creneauHoraire);
+		$this->execute($requete);
+	}
+	
+	/**
+     * @param $idMedecin ID du médecin sélectionné
+     * Consulte les rendez-vous du médecin sélectionné
+     */
+	public function consulteRendezVousLibres($idMedecin)
+	{
+		$requete = $this->sendRequest('SELECT * FROM consultationMedecinLibre WHERE idMedecin=:idMedecin')
+		$requete->bindValue(':idMedecin', $idMedecin);
+		return $this->execute($requete);
+	}
+	
+	/**
+     * @param $creneauHoraire Créneau horaire du rendez-vous à ajouter
+     * Ajoute un rendez-vous pour le médecin connecté
+     */
+	public function ajouteRendezVousMedecin($creneauHoraire)
+	{
+		$requete = $this->sendRequest('INSERT INTO Consultation (idMedecin, creneauHoraire) VALUES (:idMedecin, :creneauHoraire)')
+		$requete->bindValue(':idMedecin', $_SESSION['idPersonne']);
+		$requete->bindValue(':creneauHoraire', $creneauHoraire);
+		$this->execute($requete);
+	}
+	
+	/**
+     * @param $creneauHoraire Créneau horaire du rendez-vous à ajouter
+     * Supprime un rendez-vous pour le médecin connecté
+     */
+	public function supprimeRendezVousMedecin($creneauHoraire)
+	{
+		$requete = $this->sendRequest('DELETE FROM Consultation WHERE idMedecin=:idMedecin AND creneauHoraire=:creneauHoraire;')
+		$requete->bindValue(':idMedecin', $_SESSION['idPersonne']);
+		$requete->bindValue(':creneauHoraire', $creneauHoraire);
+		$this->execute($requete);
+	}
+	
+	/**
+     * Consulte les rendez-vous du médecin connecté
+     */
+	public function consulteRendezVousMedecin()
+	{
+		$requete = $this->sendRequest('SELECT * FROM consultationPatient WHERE idMedecin=:idMedecin')
+		$requete->bindValue(':idMedecin', $_SESSION['idPersonne']);
+		return $this->execute($requete);
+	}
+	
 }
 ?>
