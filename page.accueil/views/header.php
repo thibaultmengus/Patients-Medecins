2e644406ab158dba5965695ee554c5439a912e57
Okami <pierrick.garcia@hotmail.fr>
2018-11-14 14:29:18 +0100

    Rename page.acceuil to page.accueil

--- page.accueil/views/header.php
+++ page.accueil/views/header.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="./CSS/calendar.css">
-    <link rel="stylesheet" type="text/css" href="../../CSS/menu_deroulant_acceuil_patient.css">
+    <link rel="stylesheet" type="text/css" href="../../CSS/menu_deroulant_accueil_patient.css">
 </head>
 
 <body>
