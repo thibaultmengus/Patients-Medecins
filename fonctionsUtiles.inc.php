f70589a9ceea9441ddfbb7c9a39c491aaa51e0ed
Okami <pierrick.garcia@hotmail.fr>
2018-11-14 15:55:13 +0100

    Readd debug_to_console

--- fonctionsUtiles.inc.php
+++ fonctionsUtiles.inc.php
@@ -3,7 +3,8 @@
 // Mettre du JS
 // Si l'utilisateur ne laisse pas les champs vides ou seulement avec des espaces
 // ou égale à null, la fonction renvoie false, sinon true
-function formulaireBienRempli($tabPost) {
+function formulaireBienRempli($tabPost)
+{
     foreach ($tabPost as $val) {
         if (empty(trim($val)) || $val == null)
             return false;
@@ -11,4 +12,10 @@ function formulaireBienRempli($tabPost) {
     return true;
 }
 
+function debug_to_console($data) {
+ 	$output = $data;
+ 	if (is_array($output))
+ 		$output = implode(',', $output);
+	echo "<script>console.log('Debug Objects:" . $output . "');</script>";
+}
 ?>
